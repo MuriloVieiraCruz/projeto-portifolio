1bb87e0e3e2c8c70638c96a28cdb7914cb7c4f89
Murilo Vieira <murilo12super@gmail.com>
2023-08-20 00:09:02 -0300

    NavBar finalizado e css finalizado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,12 @@
-import { useState, useEffect } from "react"
-import { Navbar, Container } from "react-bootstrap"
+import { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/images/logo.svg';
+import navIcon1 from '../assets/images/nav-icon1.svg';
+import navIcon2 from '../assets/images/nav-icon2.svg';
+import navIcon3 from '../assets/images/nav-icon3.svg';
+
 export const NavBar = () => {
+
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, seScrolled] = useState(false);
 
@@ -25,8 +31,8 @@ export const NavBar = () => {
     return (
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="#home">
-            <img src={''} alt="logo" />
+          <Navbar.Brand href="/">
+            <img src={logo} alt="logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggle-icon"></span>
@@ -44,10 +50,10 @@ export const NavBar = () => {
                 onClick={() => onUpdateActiveLink('projects')}>Projetos</Nav.Link>
             </Nav>
             <span className="navbar-text">
-                <div className="social-icons">
-                    <a href="#"><img src={''} alt="" /></a>
-                    <a href="#"><img src={''} alt="" /></a>
-                    <a href="#"><img src={''} alt="" /></a>
+                <div className="social-icon">
+                    <a href="#"><img src={navIcon1} alt="" /></a>
+                    <a href="#"><img src={navIcon2} alt="" /></a>
+                    <a href="#"><img src={navIcon3} alt="" /></a>
                 </div>
                 <button className="vvd" onClick={() => console.log('conectado')}><span>Vamos nos conectar</span></button>
             </span>
